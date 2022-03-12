64c02a3870735c116074d109ebcfb852e48573a6
Michael Ragazzon <michael.ragazzon@gmail.com>
2022-03-12 23:55:20 +0100

    PlatformExtensions default file extension

--- Samples/shell/PlatformExtensions.h
+++ Samples/shell/PlatformExtensions.h
@@ -36,7 +36,7 @@ namespace PlatformExtensions {
 Rml::String FindSamplesRoot();
 
 Rml::StringList ListDirectories(const Rml::String& in_directory);
-Rml::StringList ListFiles(const Rml::String& in_directory, const Rml::String& extension);
+Rml::StringList ListFiles(const Rml::String& in_directory, const Rml::String& extension = Rml::String());
 
 } // namespace PlatformExtensions
 
