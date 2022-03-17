e471001e001c3295744ca618097480ce2562c5eb
Michael Ragazzon <michael.ragazzon@gmail.com>
2022-03-17 01:06:33 +0100

    Minor fix

--- Backends/RmlUi_Platform_SDL.h
+++ Backends/RmlUi_Platform_SDL.h
@@ -38,7 +38,7 @@ class SystemInterface_SDL : public Rml::SystemInterface {
 public:
 	double GetElapsedTime() override;
 
-	void SetMouseCursor(const Rml::String& cursor_name);
+	void SetMouseCursor(const Rml::String& cursor_name) override;
 
 	void SetClipboardText(const Rml::String& text) override;
 	void GetClipboardText(Rml::String& text) override;
